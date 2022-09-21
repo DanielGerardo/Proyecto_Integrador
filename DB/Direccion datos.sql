1c8c3fb7af9372e70f24aec8d1e213018903d6ee
DanielGerardo <danyjakfg@live.com.mx>
2022-09-20 21:44:45 -0500

    Se agrego la base de datos a una carpeta

---
+++ DB/Direccion datos.sql
@@ -0,0 +1,10 @@
+use popcollectordb;
+
+select* from direccion;
+ -- delete from direccion;
+
+insert into `direccion` values
+(null,"Camino del sol",154,"La luna","Vía Lactea","Galaxia",64114,1234567890,1);
+
+insert into `direccion` values
+(null,"Camino",154,"de la vida","son muy dificil","de andarlos",64214,1234567891,2);
